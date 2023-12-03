4c6d86b9a07a2c326e2f7ebf4b15e18932b6c290
Samuele Mazzei <samumaz01@gmail.com>
2023-12-03 16:11:03 +0100

    Fixed routing

--- test/pannernode/vite.config.js
+++ test/pannernode/vite.config.js
@@ -2,4 +2,12 @@ import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default {
   plugins: [basicSsl()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        A1: "A1.html",
+      },
+    },
+  },
 };
