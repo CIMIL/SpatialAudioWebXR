d3daa8235f1ee4545f4eaf86f89d4f1c1a059ee8
Samuele Mazzei <samumaz01@gmail.com>
2024-04-05 12:16:13 +0200

    Add AudioSrc component and update constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,4 +3,4 @@ export const DELAY_AFTER_START = 0; // TODO remove
 export const LEVELS = 3;
 export const SPEAKER_RADIUS = 2;
 export const DEBUG = true;
-export const BASELINE_WAIT_TIME = 10000;
+export const BASELINE_WAIT_TIME = 3000; // TODO increase after testing
