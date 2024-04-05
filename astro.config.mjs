186985134561e1f481a6b56cebd5ff983443567a
Samuele Mazzei <samumaz01@gmail.com>
2024-04-05 10:35:37 +0200

    added https to astro project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    plugins: [basicSsl()],
     resolve: {
       alias: {
         "@": "./src",
