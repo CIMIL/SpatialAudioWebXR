92c5ef7e497d164500384b11bfc55f7fb52dc043
Samuele Mazzei <samumaz01@gmail.com>
2024-07-22 11:17:18 +0200

    updated turns and debug const

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const TIME_BETWEEN_TURNS = 3000;
-export const DEBUG = true;
-export const TURNS = 3;
+export const DEBUG = false;
+export const TURNS = 10;
 export const SPEAKER_RADIUS = 2;
-export const BASELINE_WAIT_TIME = 3000; // TODO increase after testing
+export const BASELINE_WAIT_TIME = 3000;
