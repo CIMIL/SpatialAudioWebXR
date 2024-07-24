2d9972927fcbb7b4e16a7656c328a6e37884f918
Samuele Mazzei <samumaz01@gmail.com>
2024-07-24 11:51:01 +0200

    removed test url and key

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,5 @@ export const TURNS = 10;
 export const SPEAKER_RADIUS = 2;
 export const BASELINE_WAIT_TIME = 3000;
 export const VOLUME = 5;
-export const SUPABASE_URL = "https://smzlrhdsonkphxctzkkv.supabase.co";
-export const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNtemxyaGRzb25rcGh4Y3R6a2t2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE4MDgyOTksImV4cCI6MjAzNzM4NDI5OX0.njAZj0YLXU-EqN5Jwucri9AFf2Wrh0jkwBn-jabqrqc";
+export const SUPABASE_URL = "<your-url>";
+export const SUPABASE_ANON_KEY = "<your-key>";
