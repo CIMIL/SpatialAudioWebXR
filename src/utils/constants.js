19e9173a2a390a26fd697686fbef95f4ba436fa7
Samuele Mazzei <samumaz01@gmail.com>
2024-12-09 11:48:16 +0100

    Update speaker distribution logic

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,7 @@ export const TIME_BETWEEN_TURNS = 3000;
 export const DEBUG = false;
 export const TURNS = 16;
 export const SPEAKER_RADIUS = 2;
+export const SPEAKERS_COUNT = 42;
 export const BASELINE_WAIT_TIME = 3000;
 export const VOLUME = 5;
 export const SUPABASE_URL = import.meta.env.PUBLIC_SUPABASE_URL;
