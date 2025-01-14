1b8d4a5403b1588b77564f12520a54535941439b
Samuele Mazzei <samumaz01@gmail.com>
2025-01-14 12:29:40 +0100

    added audio context

--- src/utils/start-button.js
+++ src/utils/start-button.js
@@ -3,6 +3,8 @@ import { pushExperience } from "./logs";
 AFRAME.registerComponent("start-button", {
   init: function () {
     this.el.addEventListener("click", () => {
+      var audioContext = new AudioContext();
+      console.log("audioContext", audioContext);
       // push a new experience object to the data array
       // get the current experience name from location title
       const experience = document.title;
